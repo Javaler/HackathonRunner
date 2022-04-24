cebd659aa40f7f5e902d498cf42142142d1b57e9
YutaSugino <79685987+YutaSugino@users.noreply.github.com>
2022-04-24 11:54:14 +0900

    questionResult.html修正

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import com.example.demo.form.QuestionForm;
 
 
 @Controller
@@ -13,7 +12,7 @@ public class HomeController {
 	public ModelAndView showHome(ModelAndView mav) {
 		
 		mav.setViewName("home");
-		mav.addObject("home", new QuestionForm());
+		mav.addObject("home");
 		
 		return mav;
 	}
