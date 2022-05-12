c3680fdfbd3d9fa8592fe25fa0a5c26dd89fc841
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-05-13 00:13:47 +0900

    bootstrapの公式リファレンスを参考にバリデーションを修正

--- src/main/resources/static/js/questionForm.js
+++ src/main/resources/static/js/questionForm.js
@@ -1,46 +1,12 @@
-const submitButton=document.getElementById('submit-button'),
-sendButton=document.getElementById('send-button');
+const forms=document.querySelectorAll('form.needs-validated');
 
-let invalidColumns=[];
-
-function getValidationMessage(invalidColumns){
-    return invalidColumns.map((column)=>{
-        return column.getElementsByTagName('p')[0].textContent;
-    }).join(',')+'の項目が入力されていません。';
-}
-
-function getInvalidColumns(){
-    const columns=document.getElementsByClassName('column');
-    const invalidColumns=[];
-    for(const column of columns){
-        let isValid=false;
-        const radios=column.querySelectorAll('input[type=radio]');
-        for(const radio of radios){
-            isValid=isValid||radio.checked;
+for(const form of forms){
+    form.addEventListener('submit',(e)=>{
+        console.log('validation');
+        if(!form.checkValidity()){
+            e.preventDefault();
+            e.stopPropagation();
         }
-        if(!isValid){
-            invalidColumns.push(column);
-        }
-    }
-    return invalidColumns;
-}
-
-sendButton.addEventListener('click',()=>{
-    for(const column of invalidColumns){
-        column.classList.toggle('valid');
-    }
-    invalidColumns=getInvalidColumns();
-    for(const column of invalidColumns){
-        column.classList.toggle('valid');
-    }
-    if(invalidColumns.length){
-        alert(getValidationMessage(invalidColumns));
-        scrollToElement(invalidColumns[0]);
-    }else{
-        submitButton.click();
-    }
-});
-
-function scrollToElement(element){
-    scrollTo({top:element.getBoundingClientRect().top+window.pageYOffset,behavior:"smooth"});
+        form.classList.add('was-validated');
+    },false);
 }
