607ea0bb7bc08d1795d2106e3bb813a8cbface27
kmusicsports <naokik.musicsports1027@gmail.com>
2022-06-19 11:15:12 +0900

    add PostForm.java

---
+++ src/main/java/com/example/demo/form/PostForm.java
@@ -0,0 +1,14 @@
+package com.example.demo.form;
+
+import com.example.demo.entity.Post;
+
+public class PostForm {
+
+	public Post toEntity() {
+		Post post = new Post();
+		
+		
+		
+		return post;
+	}
+}
