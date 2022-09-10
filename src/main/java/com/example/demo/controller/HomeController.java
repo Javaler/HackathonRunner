d0f19ce905e8a42739877fb33add49d89bc83869
YutaSugino <79685987+YutaSugino@users.noreply.github.com>
2022-09-10 18:26:32 +0900

    edit HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,21 +2,45 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.entity.Post;
+import com.example.demo.form.HomeQuery;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.dao.HomeDaoImpl;
+import com.example.demo.entity.Post;
+import com.example.demo.form.HomeQuery;
 import com.example.demo.repository.PostRepository;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class HomeController {
 	
 	private final PostRepository postRepository;
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	TodoDaoImpl todoDaoImpl;
+
+	@PostConstruct
+	public void init() {
+	todoDaoImpl = new TodoDaoImpl(entityManager);
+	}
+	
 	
 	@GetMapping("/")
 	public ModelAndView showHome(ModelAndView mav) {
@@ -25,7 +49,23 @@ public class HomeController {
 		
 		mav.setViewName("home");
 		mav.addObject("postList", postList);
+		mav.addObject("homeQuery", new HomeQuery());
 		
 		return mav;
 	}
+	
+	@PostMapping("/home/query")
+	public ModelAndView queryTodo(@ModelAttribute HomeQuery homeQuery, 
+	                                BindingResult result,
+	                                ModelAndView mv) {
+	    mv.setViewName("homeList");
+
+	    List<Post> homeList = null;
+	    homeList = homeDaoImpl.findByCriteria(homeQuery);
+
+	    // mv.addObject("homeQuery", homeQuery);
+	    mv.addObject("homeList", homeList);
+
+	    return mv;
+	  }
 }
