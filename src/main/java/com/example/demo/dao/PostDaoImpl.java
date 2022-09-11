43daed2da190b41f4926d32d7c1272bf111f00ae
YutaSugino <79685987+YutaSugino@users.noreply.github.com>
2022-09-11 19:04:05 +0900

    edit indent of PostDaoImpl.java

--- src/main/java/com/example/demo/dao/PostDaoImpl.java
+++ src/main/java/com/example/demo/dao/PostDaoImpl.java
@@ -21,96 +21,96 @@ public class PostDaoImpl implements PostDao {
 	// Criteria APIによる検索
 	@Override
 	public List<Post> findByCriteria(PostQuery postQuery) {
-	CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-	CriteriaQuery<Post> query = builder.createQuery(Post.class);
-	Root<Post> root = query.from(Post.class);
-	List<Predicate> predicates = new ArrayList<>();
-	
-	// ハッカソン経験
-	if (postQuery.getHackathon() != -1) {
-		predicates.add(
-			builder.and(
-				builder.equal(root.get(Post_.HACKATHON), postQuery.getHackathon())));
-	}
-	
-	// チーム開発
-	if (postQuery.getTeam() != -1) {
-	    predicates.add(
-	        builder.and(
-	            builder.equal(root.get(Post_.TEAM), postQuery.getTeam())));
-	}
-	
-	// ツール開発
-	if (postQuery.getPortfolio() != -1) {
-	    predicates.add(
-	    	builder.and(
-	    		builder.equal(root.get(Post_.PORTFOLIO), postQuery.getPortfolio())));
-	}
-	
-	// git経験
-	if (postQuery.getGit() != -1) {
-	    predicates.add(
-	    	builder.and(
-	    		builder.equal(root.get(Post_.GIT), postQuery.getGit())));
-	}
-	
-	// 動画編集経験
-	if (postQuery.getMovie() != -1) {
-	    predicates.add(
-	    	builder.and(
-	    		builder.equal(root.get(Post_.MOVIE), postQuery.getMovie())));
-	}
-	
-	// プレゼン経験
-	if (postQuery.getPresentation() != -1) {
-	    predicates.add(
-	    	builder.and(
-	    		builder.equal(root.get(Post_.PRESENTATION), postQuery.getPresentation())));
-	}
-	
-	// デザイン
-	if (postQuery.getDesign() != -1) {
-	    predicates.add(
-	    	builder.and(
-	    		builder.equal(root.get(Post_.DESIGN), postQuery.getDesign())));
-	}
-	
-	// フロントエンド
-	if (postQuery.getFrontend() != -1) {
-	    predicates.add(
-	    	builder.and(
-	    		builder.equal(root.get(Post_.FRONTEND), postQuery.getFrontend())));
-	}
-	
-	// バックエンド開発
-	if (postQuery.getBackend() != -1) {
-	    predicates.add(
-	    	builder.and(
-	    		builder.equal(root.get(Post_.BACKEND), postQuery.getBackend())));
-	}
-	
-	// インフラ
-	if (postQuery.getInfrastructure() != -1) {
-	    predicates.add(
-	    	builder.and(
-	    		builder.equal(root.get(Post_.INFRASTRUCTURE), postQuery.getInfrastructure())));
-	}
-	
-	// データ解析
-	if (postQuery.getMachineLearning() != -1) {
-	    predicates.add(
-	    	builder.and(
-	    		builder.equal(root.get(Post_.MACHINELEARNING), postQuery.getMachineLearning())));
-	}
-	
-	// SELECT作成
-    Predicate[] predArray = new Predicate[predicates.size()];
-    predicates.toArray(predArray);
-    query = query.select(root).where(predArray).orderBy(builder.desc(root.get(Post_.id)));
-	   
-	    // 検索
-	List<Post> list = entityManager.createQuery(query).getResultList();
-	
-	return list;
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Post> query = builder.createQuery(Post.class);
+		Root<Post> root = query.from(Post.class);
+		List<Predicate> predicates = new ArrayList<>();
+		
+		// ハッカソン経験
+		if (postQuery.getHackathon() != -1) {
+			predicates.add(
+				builder.and(
+					builder.equal(root.get(Post_.HACKATHON), postQuery.getHackathon())));
+		}
+		
+		// チーム開発
+		if (postQuery.getTeam() != -1) {
+			predicates.add(
+				builder.and(
+		            builder.equal(root.get(Post_.TEAM), postQuery.getTeam())));
+		}
+		
+		// ツール開発
+		if (postQuery.getPortfolio() != -1) {
+		    predicates.add(
+		    	builder.and(
+		    		builder.equal(root.get(Post_.PORTFOLIO), postQuery.getPortfolio())));
+		}
+		
+		// git経験
+		if (postQuery.getGit() != -1) {
+		    predicates.add(
+		    	builder.and(
+		    		builder.equal(root.get(Post_.GIT), postQuery.getGit())));
+		}
+		
+		// 動画編集経験
+		if (postQuery.getMovie() != -1) {
+		    predicates.add(
+		    	builder.and(
+		    		builder.equal(root.get(Post_.MOVIE), postQuery.getMovie())));
+		}
+		
+		// プレゼン経験
+		if (postQuery.getPresentation() != -1) {
+		    predicates.add(
+		    	builder.and(
+		    		builder.equal(root.get(Post_.PRESENTATION), postQuery.getPresentation())));
+		}
+		
+		// デザイン
+		if (postQuery.getDesign() != -1) {
+		    predicates.add(
+		    	builder.and(
+		    		builder.equal(root.get(Post_.DESIGN), postQuery.getDesign())));
+		}
+		
+		// フロントエンド
+		if (postQuery.getFrontend() != -1) {
+		    predicates.add(
+		    	builder.and(
+		    		builder.equal(root.get(Post_.FRONTEND), postQuery.getFrontend())));
+		}
+		
+		// バックエンド開発
+		if (postQuery.getBackend() != -1) {
+		    predicates.add(
+		    	builder.and(
+		    		builder.equal(root.get(Post_.BACKEND), postQuery.getBackend())));
+		}
+		
+		// インフラ
+		if (postQuery.getInfrastructure() != -1) {
+		    predicates.add(
+		    	builder.and(
+		    		builder.equal(root.get(Post_.INFRASTRUCTURE), postQuery.getInfrastructure())));
+		}
+		
+		// データ解析
+		if (postQuery.getMachineLearning() != -1) {
+		    predicates.add(
+		    	builder.and(
+		    		builder.equal(root.get(Post_.MACHINELEARNING), postQuery.getMachineLearning())));
+		}
+		
+		// SELECT作成
+	    Predicate[] predArray = new Predicate[predicates.size()];
+	    predicates.toArray(predArray);
+	    query = query.select(root).where(predArray).orderBy(builder.desc(root.get(Post_.id)));
+		   
+		    // 検索
+		List<Post> list = entityManager.createQuery(query).getResultList();
+		
+		return list;
     }
 }
