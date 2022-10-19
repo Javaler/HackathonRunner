434725c217b5566686608e2d58b183b885546194
kmusicsports <naokik.musicsports1027@gmail.com>
2022-10-19 16:44:37 +0900

    add RecruitmentForm.java

---
+++ src/main/java/com/example/demo/form/RecruitmentForm.java
@@ -0,0 +1,15 @@
+package com.example.demo.form;
+
+import lombok.Data;
+
+@Data
+public class RecruitmentForm {
+    
+    private String name;
+    private String eventInfo;
+    private String comment;
+    private int memberNum;
+    private String deadline;
+    private String contactInfo;
+        
+}
