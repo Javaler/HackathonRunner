c91659c1b2d9125368391c733274a453b04c0f12
unknown <mulberry.kzuki@gmail.com>
2022-10-21 02:10:18 +0900

    add files I don't know

--- target/generated-sources/annotations/com/example/demo/entity/Recruitment_.java
+++ target/generated-sources/annotations/com/example/demo/entity/Recruitment_.java
@@ -10,25 +10,25 @@ import javax.persistence.metamodel.StaticMetamodel;
 @StaticMetamodel(Recruitment.class)
 public abstract class Recruitment_ {
 
-	public static volatile SingularAttribute<Recruitment, String> event_info;
-	public static volatile SingularAttribute<Recruitment, Timestamp> updated_at;
-	public static volatile SingularAttribute<Recruitment, Integer> member_num;
+	public static volatile SingularAttribute<Recruitment, Timestamp> createdAt;
+	public static volatile SingularAttribute<Recruitment, String> eventInfo;
+	public static volatile SingularAttribute<Recruitment, String> contactInfo;
 	public static volatile SingularAttribute<Recruitment, String> name;
-	public static volatile SingularAttribute<Recruitment, Timestamp> created_at;
 	public static volatile SingularAttribute<Recruitment, String> comment;
 	public static volatile SingularAttribute<Recruitment, Integer> id;
-	public static volatile SingularAttribute<Recruitment, String> contact_info;
+	public static volatile SingularAttribute<Recruitment, Integer> memberNum;
 	public static volatile SingularAttribute<Recruitment, Date> deadline;
+	public static volatile SingularAttribute<Recruitment, Timestamp> updatedAt;
 
-	public static final String EVENT_INFO = "event_info";
-	public static final String UPDATED_AT = "updated_at";
-	public static final String MEMBER_NUM = "member_num";
+	public static final String CREATED_AT = "createdAt";
+	public static final String EVENT_INFO = "eventInfo";
+	public static final String CONTACT_INFO = "contactInfo";
 	public static final String NAME = "name";
-	public static final String CREATED_AT = "created_at";
 	public static final String COMMENT = "comment";
 	public static final String ID = "id";
-	public static final String CONTACT_INFO = "contact_info";
+	public static final String MEMBER_NUM = "memberNum";
 	public static final String DEADLINE = "deadline";
+	public static final String UPDATED_AT = "updatedAt";
 
 }
 
