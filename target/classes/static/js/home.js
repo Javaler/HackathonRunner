a9c4b51f61fa784a5221469a595b47a18dbd209e
kmusicsports <naokik.musicsports1027@gmail.com>
2022-10-29 19:30:53 +0900

    remove target/classes/ and target/dependency/ from git management

--- target/classes/static/js/home.js
+++
@@ -1,8 +0,0 @@
-const popoverTriggerList = document.getElementsByClassName("popover-button");
-
-for (i = 0; i < popoverTriggerList.length; i++) {
-    popoverTriggerList[i].addEventListener("click", function () {
-        this.classList.remove("btn-outline-primary");
-        this.classList.add("btn-primary");
-    });
-}
