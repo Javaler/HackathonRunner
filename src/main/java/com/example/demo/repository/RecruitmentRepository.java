a116a420c7e0b630d7efda54c3119c7f01f08f01
kmusicsports <naokik.musicsports1027@gmail.com>
2022-10-29 20:43:36 +0900

    remove findAllByOrderByIdDesc method  from RecruitmentRepository.java

--- src/main/java/com/example/demo/repository/RecruitmentRepository.java
+++ src/main/java/com/example/demo/repository/RecruitmentRepository.java
@@ -10,6 +10,5 @@ import com.example.demo.entity.Recruitment;
 @Repository
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Integer> {
 
-    List<Recruitment> findAllByOrderByIdDesc();
     List<Recruitment> findAllByOrderByDeadlineAsc();
 }
