ef07668011abe4775a2f9b7d5a5be4a1c8c44206
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-10-30 17:40:10 +0900

    add showing popover and contact in home

--- target/classes/static/js/home.js
+++ target/classes/static/js/home.js
@@ -1,8 +1,15 @@
 const popoverTriggerList = document.getElementsByClassName("popover-button");
+const contactRowList = document.getElementsByClassName("contact-row");
+const popoverList = [];
 
-for (i = 0; i < popoverTriggerList.length; i++) {
+for (let i = 0; i < popoverTriggerList.length; i++) {
+    popoverList.push(new bootstrap.Popover(popoverTriggerList[i]));
+    contactRowList[i].classList.add("hidden");
     popoverTriggerList[i].addEventListener("click", function () {
         this.classList.remove("btn-outline-primary");
         this.classList.add("btn-primary");
+        console.log(i);
+        contactRowList[i].classList.remove("hidden");
+        popoverList[i].update();
     });
 }
