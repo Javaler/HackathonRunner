1c496f5544a2a80fdce30c4f16c1b6e966b5bc95
kmusicsports <naokik.musicsports1027@gmail.com>
2022-11-06 15:40:00 +0900

    remove unused annotation of NotNull and NotBlank  from PostForm.java

--- src/main/java/com/example/demo/form/PostForm.java
+++ src/main/java/com/example/demo/form/PostForm.java
@@ -1,57 +1,26 @@
 package com.example.demo.form;
 
 import com.example.demo.entity.Post;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class PostForm {
 
     private Integer id;
-
-    @NotBlank(message = "名前を入力してください")
     private String name;
-
-    @NotNull(message = "選択してください")
     private Integer hackathon;
-
-    @NotNull(message = "選択してください")
     private Integer team;
-
-    @NotNull(message = "選択してください")
     private Integer frontend;
-
-    @NotNull(message = "選択してください")
     private Integer backend;
-
-    @NotNull(message = "選択してください")
     private Integer infrastructure;
-
-    @NotNull(message = "選択してください")
     private Integer git;
-
-    @NotNull(message = "選択してください")
     private Integer design;
-
-    @NotNull(message = "選択してください")
     private Integer movie;
-
-    @NotNull(message = "選択してください")
     private Integer machinelearning;
-
-    @NotNull(message = "選択してください")
     private Integer portfolio;
-
-    @NotNull(message = "選択してください")
     private Integer presentation;
-
-    @NotNull(message = "作品情報を入力してください")
     private String work;
-
-    @NotNull(message = "参加イベント情報を入力してください")
     private String event;
-
     private String comment;
 
     //入力データからEntityを生成して返す
