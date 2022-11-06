020066977b77cc82a8c0dd774148ce9bd316f512
kmusicsports <naokik.musicsports1027@gmail.com>
2022-11-06 17:01:28 +0900

    fix conditional statement in createAnalysisResult  method of QuestionService.java

--- src/main/java/com/example/demo/service/QuestionService.java
+++ src/main/java/com/example/demo/service/QuestionService.java
@@ -25,85 +25,83 @@ public class QuestionService {
         int infrastructure = questionForm.getInfrastructure();
         int machinelearning = questionForm.getMachinelearning();
 
-        if(git==0) {
+        if(git == 0) {
             // 0: Git, GitHub使用経験なし
             analysisResult.add("Git, GitHubの勉強をしましょう。");
         }
 
-        if(hackathon==0) {
-            if(team==0) {
-                if(portfolio==0) {
+        if(hackathon == 0) {
+            if(team == 0) {
+                if(portfolio == 0) {
                     // 1: ハッカソン参加経験なし、チーム開発経験なし、ツール（アプリ）開発経験なし
                     analysisResult.add("当日はスライド作成やアイデアを2、3個出せるように準備しておきましょう。");
-                }else {
+                } else {
                     // 4: ハッカソン参加経験なし、チーム開発経験なし、ツール（アプリ）開発経験あり
                     analysisResult.add("Git, GitHubを使ったチーム開発方法を調べましょう。");
                     analysisResult.add("チーム内にハッカソン参加経験者やチーム開発経験者がいなくて、ツール（アプリ）開発経験のある人が他にいない場合は、リーダーの役割を検討しましょう。");
                 }
-            }else {
+            } else {
                 // 2: ハッカソン参加経験なし、チーム開発経験あり
                 analysisResult.add("チームにハッカソン参加経験者がいないならリーダーの役割を担いましょう。");
             }
-        }else {
+        } else {
             // 3: ハッカソン参加経験あり
             analysisResult.add("どんどん次のハッカソンに参加して、賞を目指しましょう！");
             analysisResult.add("今までリーダーの経験がなければリーダーの役割を担ってPMしてみましょう。");
         }
 
-        if(portfolio==1 && git==0) {
+        if(portfolio > 1 && git == 0) {
             // 5: ツール（アプリ）開発経験あり、Git, GitHub使用経験なし
             analysisResult.add("チーム内にハッカソン参加経験者やチーム開発経験者がいなくて、ツール（アプリ）開発経験のある人が他にいない場合は、リーダーの役割を検討しましょう。");
         }
 
-        if(movie==1) {
+        if(movie == 1) {
             // 6: 動画編集経験あり
             analysisResult.add("動画編集できることを伝えましょう。プレゼン資料作成などで役に立つはずです。");
         }
 
-        if(presentation==1) {
+        if(presentation == 1) {
             // 7: プレゼン経験あり
             analysisResult.add("プレゼン資料の作成や発表を積極的に担当しましょう。ハッカソンではプレゼンの出来も評価対象です。");
         }
 
-        if(design==1) {
+        if(design == 1) {
             // 8: デザインツールの基礎知識あり
             analysisResult.add("チームにデザインツールの応用レベルの人がいれば、その人のサポート役を担いましょう。");
         }
 
-        if(design==2) {
+        if(design == 2) {
             // 9: デザインツールの応用レベル
             analysisResult.add("デザイン作成ができることを伝えましょう。");
         }
 
-        if(frontend==1) {
+        if(frontend == 1) {
             // 10: JavaScriptの基礎
             analysisResult.add("JavaScriptのフレームワークを使える人がいれば、その人のサポート役を担いましょう。");
         }
 
-        if(frontend==2) {
+        if(frontend == 2) {
             // 11: JavaScriptのフレームワークを使える
             analysisResult.add("フロントエンドの開発を行えることを伝えましょう。");
         }
 
-        if(backend==1) {
+        if(backend == 1) {
             // 12: バックエンド言語の基礎あり
             analysisResult.add("使えるバックエンド言語を伝えましょう。");
         }
 
-        if(infrastructure==1) {
+        if(infrastructure == 1) {
             // 13: インフラ経験あり
             analysisResult.add("使えるインフラツールを伝えましょう。");
         }
 
-        if(machinelearning==1) {
+        if(machinelearning > 1) {
             // 14: データ分析・画像処理経験あり
             analysisResult.add("機械学習を組み込めることを伝えましょう。");
         }
 
-        if(movie==0 && presentation==0
-                && design==0 && frontend==0
-                    && backend==0 && infrastructure==0
-                        && machinelearning==0) {
+        if(movie == 0 && presentation == 0 && design == 0 && frontend == 0
+        && backend == 0 && infrastructure == 0 && machinelearning == 0) {
             // 15: 技術的技能なし
             analysisResult.add("プログラミング言語(JavaScript, PHP, Rubyなどから1つ)を勉強しておきましょう。");
         }
